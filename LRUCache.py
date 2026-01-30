6094c064e42929de2a26dc120fb9b9be3abca716
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-01-29 21:03:35 -0600

    Add LRUCache class with constructor

--- LRUCache.py
+++ LRUCache.py
@@ -4,3 +4,6 @@ class Node:
     def __init__(self, key, value):
         self.key, self.value = key, value
         self.prev = self.next = None
+        
+class LRUCache:
+    def __init__(self, capacity: int):
